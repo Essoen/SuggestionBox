5d947038a058946606ad0aa4c63630cfe6c01f54
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2015-07-30 20:05:20 +0200

    Fix dependency versioning. Add some server structure.

--- server/server.js
+++ server/server.js
@@ -2,41 +2,25 @@
  * Created by esso on 28.12.14.
  */
 
-Suggestions = new Mongo.Collection('Suggestions');
-Projects = new Mongo.Collection('Projects');
-
-if (Meteor.isServer) {
-    Meteor.startup(function () {
-        return Meteor.methods({ // dev
-            removeAllProjects: function () {
-                return Projects.remove({});
-            },
-            removeAllSuggestions: function(){
-                return Suggestions.remove({});
-            },
-            insertProject : function(projectName){
-                return Projects.insert({
-                    name: projectName,
-                    description: '',
-                    url: '',
-                    leader: ''
-                });
-            }
-        });
-    });
-
-    Suggestions.allow({
-        'insert': function(object){
-            return true;
+Meteor.startup(function () {
+    return Meteor.methods({ // dev
+        removeAllProjects: function () {
+            return Projects.remove({});
         },
-
-        'update': function(object) {
-            return true;
+        removeAllSuggestions: function(){
+            return Suggestions.remove({});
+        },
+        insertProject : function(projectName){
+            return Projects.insert({
+                name: projectName,
+                description: '',
+                url: '',
+                leader: ''
+            });
         }
     });
+});
 
-    Projects.allow({
-       'insert' : function(object) { return true; },
-       'remove' : function(object) { return true; }
-    })
+function insertTestData(){
+    
 }
